7a93f609def1e8514768c1f8595f12a85bceda48
alex <yukhymenko.oleksandr@gmail.com>
2016-09-01 18:07:24 +0300

    add exec ctx

--- src/main/scala/DummyBidder.scala
+++ src/main/scala/DummyBidder.scala
@@ -7,6 +7,7 @@ import spray.routing.SimpleRoutingApp
 import scala.concurrent.duration._
 import scala.io.StdIn
 import scala.util.Random
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object DummyBidder extends App {
 
